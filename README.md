085866c3b673069985d3823dbf3056308ca847fe
esianyo <esianicd@gmail.com>
2023-08-19 23:55:03 +0000

    the trio

---
+++ README.md
@@ -0,0 +1,10 @@
+# the three techies traversing through tech
+
+**THE TRIO**
+
+> Dorcas Tamitope
+> Constance Amatey
+> Esianyo Dzisenu
+
+_we commenced with conditions in python
+* August 18, 2023
---
+++ dict.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def practice():
+    switch = dict(
+            first = 'single',
+            second = 'twins',
+            third = 'triplets',
+            fourth = 'quadruplets'
+    )
+
+    position = 'third'
+    print(switch[position])
+
+if __name__ == "__main__": practice()
